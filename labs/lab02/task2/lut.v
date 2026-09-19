e94f732974a623b4e886a9e8bdd1cb60fdfa4e1b
Abivankenobi <abhinavnareshsharma@gmail.com>
2026-09-19 13:34:59 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -15,6 +15,7 @@ module lut #(
   reg [WIDTH-1:0] mem [0:DEPTH-1];
 
   integer i;
+  
 
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
